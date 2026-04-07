22f4d78ba0c7adda3dfe5b15d9ee1a09a6f35c76
Anton Efimenko <3995773+FerrusR@users.noreply.github.com>
2026-04-07 21:12:34 +0600

    Update apiUrl to use HTTPS in production environment

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://pathfinder-helper-dev-api.azurewebsites.net/api'
+  apiUrl: 'https://pathfinder-helper-dev-api.azurewebsites.net/api'
 };
